f3c105d5b5a142bf701233d6ba2f2056b5af83e8
25316 <25316477087@qq.com>
2024-12-19 16:31:43 +0800

    加了并行卷积神经网络

---
+++ CNN.py
@@ -0,0 +1,19 @@
+
+
+import torch
+
+input = [
+    [3, 4, 6, 5, 7],
+    [2, 4, 6, 8, 2],
+    [1, 6, 7, 8, 4],
+    [9, 7, 4, 6, 2],
+    [3, 7, 5, 4, 1]
+]
+
+input = torch.Tensor(input).view(1, 1, 5, 5)  # B, C, W, H
+print(input)
+conv_layer = torch.nn.Conv2d(1, 1, kernel_size=3, padding=1, bias=False)
+
+kernel = torch.Tensor([1, 2, 3, 4, 5, 6, 7, 8, 9]).view(1, 1, 3, 3)
+conv_layer.weight.data = kern
+
